909c82b306dcc6a2b5033eb2d5677d5aa12775cf
Maksym Karalash <Maks.karalash@gmail.com>
2023-09-22 00:36:28 +0300

    slider for gallery section done for tablet

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MainTitle from "../../common/MainTitle/MainTitle";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Parallax, EffectCreative } from "swiper/modules";
 import "./Gallery.css";
 
 import images from "../../data/gallery.json";
@@ -8,23 +9,49 @@ import images from "../../data/gallery.json";
 const Gallery = () => {
   return (
     <section>
-      <MainTitle>
+      <MainTitle className="mb-[88px] text-center">
         Our <span>Gallery</span>
       </MainTitle>
       <Swiper
-        className="h-[1000px]"
-        slidesPerView={3}
+        // className="h-[1000px]"
+        slidesPerView={1.7}
         grabCursor={true}
         centeredSlides={true}
         speed={400}
         loop={true}
-        direction="vertical"
         slideActiveClass="active-slide-test"
         slideNextClass="next-slide-test"
         slidePrevClass="prev-slide-test"
+        // parallax={true}
+        effect="creative"
+        creativeEffect={{
+          limitProgress: 3, // slides after 2nd before/after active will have same state
+          prev: {
+            // opacity: not_active_slide_opacity_value,
+            scale: 0.292,
+            // will set `translateX(-95%)` on previous slides
+            translate: ["-70%", 0, 0],
+          },
+          next: {
+            // opacity: not_active_slide_opacity_value,
+            scale: 0.292,
+            // will set `translateX(95%)` on next slides
+            translate: ["70%", 0, 0],
+          },
+        }}
         onSwiper={(swiper) => console.dir(swiper)}
+        modules={[Parallax, EffectCreative]}
+        breakpoints={{
+          480: { direction: "vertical" },
+          768: {
+            direction: "horizontal",
+            // spaceBetween: 24,
+          },
+          1280: {},
+        }}
       >
         {images.map(({ id, url, alt }) => (
+          //
           <SwiperSlide className="slide-test" key={id}>
             <div>
               <img src={url} alt={alt} />
